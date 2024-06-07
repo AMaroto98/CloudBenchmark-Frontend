b3edafb8b90dfe158cac8c708a70b3c33e0ad324
Ángel Cruz <angelcruzblasco@gmail.com>
2024-06-07 22:31:52 +0200

    feat(infra): create frontend infrastructure and use workspaces to avoid code duplication

---
+++ terraform/frontend/cloudfront.tf
@@ -0,0 +1,156 @@
+data "aws_s3_bucket" "bucket" {
+  bucket = local.bucket_name
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket_public_access" {
+  bucket                  = data.aws_s3_bucket.bucket.id
+  block_public_policy     = true
+  block_public_acls       = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_cloudfront_origin_access_identity" "cloudfront_identity" {
+  comment = "Some comment"
+}
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions = ["s3:GetObject"]
+
+    resources = [
+      data.aws_s3_bucket.bucket.arn,
+      "${data.aws_s3_bucket.bucket.arn}/*"
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.cloudfront_identity.iam_arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = data.aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.s3_policy.json
+}
+
+resource "aws_cloudfront_distribution" "cloudfront_distribution" {
+  aliases             = local.aliases
+  comment             = local.comentario
+  default_root_object = "index.html"
+  enabled             = true
+  http_version        = "http2"
+  is_ipv6_enabled     = false
+  price_class         = "PriceClass_100"
+  retain_on_delete    = false
+  tags = {
+    Entorno  = local.tag_entorno
+    Proyecto = local.tag_proyecto
+  }
+  tags_all = {
+    Entorno  = local.tag_entorno
+    Proyecto = local.tag_proyecto
+  }
+
+  wait_for_deployment = true
+
+  custom_error_response {
+    error_caching_min_ttl = 300
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 0
+    max_ttl                = 0
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = data.aws_s3_bucket.bucket.id
+    trusted_key_groups     = []
+    trusted_signers        = []
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  ordered_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = false
+    default_ttl            = 0
+    max_ttl                = 0
+    min_ttl                = 0
+    path_pattern           = "index.html"
+    smooth_streaming       = false
+    target_origin_id       = data.aws_s3_bucket.bucket.id
+    trusted_key_groups     = []
+    trusted_signers        = []
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      headers                 = []
+      query_string            = false
+      query_string_cache_keys = []
+
+      cookies {
+        forward           = "none"
+        whitelisted_names = []
+      }
+    }
+  }
+
+  ordered_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = false
+    default_ttl            = 0
+    max_ttl                = 0
+    min_ttl                = 0
+    path_pattern           = "json/*"
+    smooth_streaming       = false
+    target_origin_id       = data.aws_s3_bucket.bucket.id
+    trusted_key_groups     = []
+    trusted_signers        = []
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      headers                 = []
+      query_string            = false
+      query_string_cache_keys = []
+
+      cookies {
+        forward           = "none"
+        whitelisted_names = []
+      }
+    }
+  }
+
+  origin {
+    connection_attempts = 3
+    connection_timeout  = 10
+    domain_name         = data.aws_s3_bucket.bucket.bucket_regional_domain_name
+    origin_id           = data.aws_s3_bucket.bucket.id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.cloudfront_identity.cloudfront_access_identity_path
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      locations        = []
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn            = local.certificate
+    cloudfront_default_certificate = false
+    minimum_protocol_version       = "TLSv1.2_2021"
+    ssl_support_method             = "sni-only"
+  }
+}
---
+++ terraform/frontend/variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  default     = "eu-west-1"
+}
+
+variable "dns_zone_id" {
+  default     = "Z015966142O4NTW0JJSQ"
+}
